3408c19005e069f8cd2443a3b3fe66c8db9f74f0
Paulo Guilherme <31107814+pgsilva@users.noreply.github.com>
2019-04-10 11:16:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 const http = require("http").Server(app);
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 const route = require("./routes/route");
 const chalk = require('chalk');
 const ip = require('./ip')
+const morgan = require('morgan')
 const requestIp = require('request-ip');
 require('dotenv').config()
 
@@ -18,10 +20,15 @@ app.use(
         extended: false
     })
 );
-
+app.use(bodyParser.text({ type: 'text/html' }))
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: false }))
+app.use(cookieParser())
 // parse application/json
 app.use(bodyParser.json());
 app.use(requestIp.mw());
+app.use(morgan('dev'))
+app.use("/morales/", route);
 
 //ENDPOINTS CONFIG
 let logger = (req, res, next) => {
@@ -32,7 +39,20 @@ let logger = (req, res, next) => {
     next();
 };
 app.use(logger);
-app.use("/morales/", route);
+// Tratamento de erro genérico
+app.use(function (err, req, res, next) {
+    res.locals.message = err.message
+    console.log(err)
+
+    if (err.errorView) {
+        console.log('error');
+    } else {
+        res.status(err.status || 500).send({
+            msg: "I'm dead",
+            cause: err
+        });
+    }
+});
 
 http.listen(port, () =>
     console.log(chalk.blue(Date(Date.now())
